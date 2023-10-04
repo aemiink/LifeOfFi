a0dada979f693255df3ff0040bb344c484143dc6
Emin <102097443+aemiink@users.noreply.github.com>
2023-10-04 23:18:53 +0300

    Update FiController.cs

--- FiController.cs
+++ FiController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class FiController : MonoBehaviour
 {
     [SerializeField] float movementSpeed = 5f;
     float normallySpeed;
