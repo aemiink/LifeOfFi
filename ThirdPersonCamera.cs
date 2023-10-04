a32f1bb97844e0156a49f252fb5f0595e6d78b96
Emin <102097443+aemiink@users.noreply.github.com>
2023-10-04 23:16:03 +0300

    Update FoxCamera.cs

--- ThirdPersonCamera.cs
+++ ThirdPersonCamera.cs
@@ -1,21 +1,24 @@
 using UnityEngine;
 
-public class ThirdPersonCamera : MonoBehaviour 
-{        
+public class Fox_Camera : MonoBehaviour
+{
     [SerializeField] GameObject player;
-    [SerializeField][Range(0.5f, 2f)]
-    float mouseSense = 1; 
-    [SerializeField][Range(-20, -10)]
+    [SerializeField]
+    [Range(0.5f, 2f)]
+    float mouseSense = 1;
+    [SerializeField]
+    [Range(-20, 0)]
     int lookUp = -15;
-    [SerializeField][Range(15, 25)]
+    [SerializeField]
+    [Range(15, 25)]
     int lookDown = 20;
 
-    private void Start() 
+    private void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked; 
+        Cursor.lockState = CursorLockMode.Locked;
     }
     void Update()
-    {     
+    {
         float rotateX = Input.GetAxis("Mouse X") * mouseSense;
         float rotateY = Input.GetAxis("Mouse Y") * mouseSense;
 
@@ -24,12 +27,12 @@ public class ThirdPersonCamera : MonoBehaviour
 
         rotCamera.x = (rotCamera.x > 180) ? rotCamera.x - 360 : rotCamera.x;
         rotCamera.x = Mathf.Clamp(rotCamera.x, lookUp, lookDown);
-        rotCamera.x -= rotateY;               
-        
+        rotCamera.x -= rotateY;
+
         rotCamera.z = 0;
         rotPlayer.y += rotateX;
 
         transform.rotation = Quaternion.Euler(rotCamera);
-        player.transform.rotation = Quaternion.Euler(rotPlayer);     
+        player.transform.rotation = Quaternion.Euler(rotPlayer);
     }
 }
